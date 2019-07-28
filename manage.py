cd4255ca8095891d9be035f886fc96234f373626
andres1181 <andreslozano1118@gmail.com>
2019-07-28 14:47:58 -0500

    Configuración archivo settings y creacion de archivos para la configuración en produción y desarrollo

--- manage.py
+++ manage.py
@@ -5,7 +5,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tg_OVA.settings')
+    if 'ENV_PRODUCTION' in os.environ:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tg_OVA.production')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tg_OVA.development')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- tg_OVA/settings.py
+++ tg_OVA/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'j%3kqm66x39+gmvf9egsy4!9!=!%hnvp$07xm3_!0839+6p&^$'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -69,18 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tg_OVA.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -103,9 +90,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
