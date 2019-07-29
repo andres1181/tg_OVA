cafd7bdda31ac6fd2d4afd6f8b1e5c404635b979
andres1181 <andreslozano1118@gmail.com>
2019-07-28 23:44:38 -0500

    Modificación Archivo de Inicio de los OVAS

--- Gestion_OVA/views.py
+++ Gestion_OVA/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def index(resquest):
     return render(resquest, 'index.html', {})
+
+#def index(resquest):
+#    return render_to_response('index.html')
